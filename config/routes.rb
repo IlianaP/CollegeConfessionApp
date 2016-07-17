08223a667721c103f0332cc0e8dde10c86061be4
IlianaP <ip97@cornell.edu>
2016-07-17 18:43:11 +0000

    Adding a headline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-College::Application.routes.draw do
+College::Application.routes.draw do 
+  root "confessions#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
